488d7466eb6a8d25318124e660738bd106f94279
ignxcy <ignacyy@proton.me>
2023-06-09 07:21:06 +0200

    update background.js

--- background.js
+++ background.js
@@ -1,10 +1,8 @@
-// github.com/jishanshaikh4/brave-search
-// License: MIT
-// v1.0.1, last updated: May 2022
+// This is a modified fork of https://github.com/jishanshaikh4/brave-search!!!
 
 chrome.omnibox.onInputEntered.addListener((text) => {
   var newURL =
     "https://duckduckgo.com/?q=" +
     encodeURIComponent(text);
-  chrome.tabs.create({ url: newURL });
+  chrome.tabs.update({ url: newURL });
 });
